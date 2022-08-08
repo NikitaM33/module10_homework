eeadb54c3cc9bbb312f0fd33948570ca523b1915
nikita <super.tyler33@live.com>
2022-08-08 20:27:03 +0300

    module 10

--- task_1.js
+++ task_1.js
@@ -14,25 +14,4 @@ window.addEventListener('load', function() {
             }
         }
     }
-
-
-
-
-    // Задание 2 ====================
-    // let x = true;
-
-    // switch (typeof x) {
-    //     case 'number':
-    //         console.log('x - число');
-    //         break;
-    //     case 'string':
-    //         console.log('x - строка');
-    //         break;
-    //     case 'boolean':
-    //         console.log('x - логический тип');
-    //         break;
-            
-    //     default:
-    //         console.log('Тип переменной не определён');
-    // }
 });
